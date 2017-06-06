251b3d2fd8a6367180ff3cdecb154c1daeb170c1
Timo Myllymäki <timo.myllymaki@digia.com>
2017-06-06 16:16:08 +0300

    Add Routes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <section class="app">
     <h1>Image Browser</h1>
+    <p>
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
+    </p>
+    <router-view></router-view>
   </section>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
-new Vue({ el: '#app', render: h => h(App) })
+Vue.use(VueRouter)
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+new Vue({ el: '#app', render: h => h(App), router })
