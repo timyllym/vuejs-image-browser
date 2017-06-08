e79ed5ab8b5f2490893e4750d87a3a955a77f975
Timo Myllymaki <timo.myllymaki@digia.com>
2017-06-08 16:04:22 +0300

    Add vuex-route-sync

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import { sync } from 'vuex-router-sync'
 import App from './App.vue'
 import router from './routes'
 import store from './store'
 
+sync(store, router)
 new Vue({ el: '#app', render: h => h(App), router, store })
