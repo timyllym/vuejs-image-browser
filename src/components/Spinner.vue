f83e753301e2dcda34e911425a664453df24d83d
Timo Myllymaki <timyllym@gmail.com>
2017-06-11 14:47:12 +0300

    Add loading spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class='spinner-container'>
+    <div :class='{ "spinner": true, "dark": isDark }' />
+    <div class='message'>{{ message }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['message', 'isDark']
+  }
+</script>
+
+<style lang='scss' scoped>
+  @import '../constants';
+  .spinner-container {
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  .message {
+    display: inline-block;
+    vertical-align: middle;
+    color: $spinner-text-color;
+    cursor: default;
+  }
+
+  .spinner {
+    background-image: url('../images/spinner.png');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+    width: 16px;
+    height: 16px;
+    margin-left: 0px;
+    margin-right: 7px;
+    flex: 1 1 auto;
+    animation: spin 0.7s linear infinite;
+    display: inline-block;
+    vertical-align: middle;
+    &.hidden {
+      display: none;
+    }
+    &.dark {
+      background-image: url('../images/spinner_black.png');
+    }
+  }
+
+  @keyframes spin { 100% { transform:rotate(360deg); } }
+</style>
--- src/components/TreeView.vue
+++ src/components/TreeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <spinner message='Loading...' is-dark='true' v-if='images.length == 0' class='spinner-box' />
     <div class='thumbnail-group'>
       <div class='thumbnail' v-for='image in images'>
         <img :src='image.url' @click='openImage(image)' />
@@ -11,8 +12,12 @@
 
 <script>
   import { mapGetters, mapActions } from 'vuex'
+  import Spinner from './Spinner.vue'
 
   export default {
+    components: {
+      'spinner': Spinner
+    },
     computed: mapGetters({
       images: 'imageList'
     }),
@@ -25,3 +30,10 @@
     }
   }
 </script>
+
+<style lang='scss' scoped>
+  .spinner-box {
+    margin-left: 20px;
+    margin-top: 10px;
+  }
+</style>
